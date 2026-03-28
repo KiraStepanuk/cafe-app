4456888a1d87222e3b3135f5542aba40cb8ce358
KiraStepanuk <valorantlol1@gmail.com>
2026-03-28 15:00:02 +0200

    KAN-10 Фрейм 740_v1

--- cafe-project/client/src/pages/Home.js
+++ cafe-project/client/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../components/Hero/Hero';
 import FlashSales from '../components/FlashSales/FlashSales';
+import Featured from '../components/Featured/Featured';
 
 const Home = () => {
   return (
     <main>
       <Hero />
       <FlashSales />
+      <Featured />
     </main>
   );
 };
